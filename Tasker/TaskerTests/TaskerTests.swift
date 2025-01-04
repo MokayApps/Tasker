3a58517b47ac24b52cae657ce7ca8b46e78b3558
Andrei Kozlov <akozlov@tradingview.com>
2025-01-04 14:55:32 +0100

    created xcode project

---
+++ Tasker/TaskerTests/TaskerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerTests.swift
+//  TaskerTests
+//
+//  Created by Andrei Kozlov on 4/1/25.
+//
+
+import Testing
+@testable import Tasker
+
+struct TaskerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
